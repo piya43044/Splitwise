1d6a429cccc6dbd9fe93c9f7573e2fd65ae04ee7
Priya <agrawalpriya324@gmail.com>
2023-03-31 16:35:51 +0530

    Create login design

--- src/app/user-sign-up/login/login.component.ts
+++ src/app/user-sign-up/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+
+  ngOnInit(){
+    this.loginForm = new FormGroup({
+        email : new FormControl('', [Validators.required , Validators.email]),
+        password : new FormControl('',[Validators.required])
+    })
+  }
+
+  get email(){
+    return this.loginForm.get('email');
+  }
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+  onSubmit(){
+    console.log(this.loginForm);
+    this.loginForm.reset();
+  }
 
 }
