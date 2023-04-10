37a204c7be2418fe844f716fbe87209d81ec4a67
Priya <agrawalpriya324@gmail.com>
2023-04-11 00:05:15 +0530

    Create transaction list design

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class TransactionComponent {
 
+  transactionList = [
+    { payer:'Priya', receiver:'Mayank',currency:'USD', amount:1000},
+    { payer:'Harish', receiver:'Mayank',currency:'USD', amount:2000},
+    { payer:'Nikita', receiver:'Priya',currency:'₹' , amount:3000}
+  ];
+  
 }
