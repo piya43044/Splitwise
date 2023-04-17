a4622242108a9d046fbcd0484c9f40ff4b97a47c
Priya <agrawalpriya324@gmail.com>
2023-04-17 11:25:38 +0530

    Create settle up design and changes in routing

--- src/app/settle-up/settle-up.module.ts
+++ src/app/settle-up/settle-up.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OweListComponent } from './owe-list/owe-list.component';
-import { TransactionPatternComponent } from './transaction-pattern/transaction-pattern.component';
 import { SettleUpComponent } from './settle-up.component';
 import { SettleUpRoutingModule } from './settle-up-routing.module';
 
 
 @NgModule({
   declarations: [
-    OweListComponent,
-    TransactionPatternComponent,
     SettleUpComponent
   ],
   imports: [
     CommonModule,
     SettleUpRoutingModule
-  ],
-  exports: [
-    OweListComponent,
-    TransactionPatternComponent
   ]
 })
 export class SettleUpModule { }
