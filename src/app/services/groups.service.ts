0ac85a598447d5c7b9d176ef472ed324f3d77fcc
Ajay Dhamuniya <aaa.dhamuniya@gmail.com>
2023-06-19 10:42:48 +0530

    This commit will add some testing changes

--- src/app/services/groups.service.ts
+++ src/app/services/groups.service.ts
@@ -11,6 +11,7 @@ import { GroupItem } from '../models/groupItem.model';
 })
 export class GroupsService {
   group !: Groups;
+  groupList !: GroupList[];
   getSelectedGroupIndex !: number;
 
   constructor(
